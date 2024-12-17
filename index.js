1197a768a8cb4fae55f3b032ac997e50f7c43c6e
Er.Krishna Kumar <135963785+Krishnarov@users.noreply.github.com>
2024-12-18 02:58:19 +0530

    cors origin cheng

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://webseederfrontendbykrishna.netlify.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }))
