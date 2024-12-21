fb3a5370325bdb0454cdf2c36af968f73435b3c4
Er.Krishna Kumar <135963785+Krishnarov@users.noreply.github.com>
2024-12-21 12:19:16 +0530

    Single Device Login Restriction

--- Middleware/authMiddleware.js
+++ Middleware/authMiddleware.js
@@ -1,13 +1,11 @@
 import User from "../Models/User_model.js";
 
 import jwt from "jsonwebtoken";
- 
 
 export const isAuthenticated = async (req, res, next) => {
-  const token =req.headers.authorization?.split(" ")[1];
+  const token = req.headers.authorization?.split(" ")[1];
 
-  
-  if (!token) return res.status(401).json({ message: "Not authorized"});
+  if (!token) return res.status(401).json({ message: "Not authorized" });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_KEY);
@@ -16,8 +14,9 @@ export const isAuthenticated = async (req, res, next) => {
       return res.status(401).json({ messeage: "invalied token", token: token });
     }
     const user = await User.findById(decoded.userId);
-
-
+    if (user.currentToken !== token) {
+      return res.status(403).json({ message: "user logged out " });
+    }
     req.user = user;
     next();
   } catch (error) {
